72395b2157b69ae77a0deebdc93c20bca3b70d4f
luizmathias1 <luizmathiasrivabem@gmail.com>
2026-03-12 23:52:20 -0300

    feat: função para realizar a leitura dos arquivos.txt

--- src/exibirResultados.py
+++ src/exibirResultados.py
@@ -1,12 +1,9 @@
 # Alertar se o arquivo tiver linhas malformadas ou exceder limites.
+import parseExpressao
 
 def ler_arquivo(arquivo):
     with open(arquivo, 'r') as f:
         linhas = f.readlines()
-    
-    for i, linha in enumerate(linhas):
-        if len(linha) > 200:
-            raise ValueError(f"Erro: Linha {i+1} excede o limite de 200 caracteres.")
     
     print(f"Arquivo '{arquivo}' lido com sucesso. Total de linhas: {len(linhas)}")
 
@@ -14,5 +11,5 @@ def ler_arquivo(arquivo):
     for linha in linhas:
         formatado.append(linha.strip("\n"))
 
-    print(formatado)
     return formatado
+
