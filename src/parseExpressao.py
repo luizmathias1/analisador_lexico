2500af71d793ab434cb555e6d6827c75771d99db
Bruno Teider <brunoteider@hotmail.com>
2026-03-14 13:50:35 -0300

    feat: :sparkles: added operator quantity to make operations correctly

--- src/parseExpressao.py
+++ src/parseExpressao.py
@@ -21,14 +21,18 @@ def printEstado(state_name, char, lista, index, color):
 def printTokenConcluido(tokens):
     print(f"{GREEN}lista concluida -> tokens: {tokens}{RESET}")
 
-erroText = f"{RED}Erro: "
+erroText = f"{RED}Erro:"
 
 # -- Execução principal --
-balance = 0
+balance = 0 # Usado para fazer balanceamento de parênteses, incrementa para '(' e decrementa para ')'
+
+# Validar se operadores tem operandos suficientes, caso ((3 4 +) (2 1 -) *), cada resultado se torna um operando para *
+qnt_operandos = 0 
+
 def parseExpressao(linha):
-    print(f'\n-- Processing line "{linha}" --')
-    global balance
-    balance = 0
+    print(f'\n-- Processing line "{linha}"')
+    global balance, qnt_operandos
+    balance, qnt_operandos = 0, 0
 
     tokens = []
     if not isinstance(linha, str): raise TypeError("parseExpressao espera uma string como 'linha'")
@@ -55,7 +59,7 @@ def estadoInicial(char, lista, tokens, linha, index):
             return True
         except:
             return False
-
+        
     printEstado("estadoInicial", char, lista, index, BLUE)
     if char.isspace(): 
         return estadoInicial, ""
@@ -76,12 +80,13 @@ def estadoInicial(char, lista, tokens, linha, index):
         else:
             return estadoOperador(char, lista, tokens, linha, index)
 
-    # Tratamento de operadores. É esperado ter dois números antes do operador.
+    # Tratamento de operadores. É esperado ter dois operandos antes do operador.
     if char in "+*/^%":
-        if len(tokens) >= 2 and is_num(tokens[-1]) and is_num(tokens[-2]):
+        global qnt_operandos
+        if qnt_operandos >= 2:
             return estadoOperador(char, lista, tokens, linha, index)
         else:
-            raise ValueError(f"{erroText} operador '{char}'. Esperado dois números antes do operador na posição {index}{RESET}")
+            raise ValueError(f"{erroText} operador '{char}'. Esperado dois operandos antes do operador na posição {index}{RESET}")
 
     # As letras formam os comandos especiais (RES) ou variveis de memória (MEM, X, Y)
     if char.isalpha():
@@ -109,12 +114,13 @@ def estadoNumero(char, lista, tokens, linha, index):
     # Verificação de segurança: evita salvar lixo como número
     if lista == '-' or lista == '.':
         raise ValueError(f"{erroText} sequência inválida tentando formar número falhou, sobrando apenas um: '{lista}'{RESET}")
-    
-    
         
     # Salvar o número completo na lista de tokens, já que o próximo char não é mais parte do número
     tokens.append(lista)
     printTokenConcluido(tokens)
+
+    global qnt_operandos
+    qnt_operandos += 1 # Números sao operandos
     
     # Repassa o caractere atual pro estado inicial, assim comecando uma nova lista
     return estadoInicial(char, "", tokens, linha, index)
@@ -133,6 +139,8 @@ def estadoComando(char, lista, tokens, linha, index):
 
 def estadoOperador(char, lista, tokens, linha, index):
     printEstado("estadoOperador", char, lista, index, YELLOW)
+    global qnt_operandos
+
     # Checar divisão inteira
     if lista == '/':
         if char == '/':
@@ -154,11 +162,18 @@ def estadoOperador(char, lista, tokens, linha, index):
         if char in "+-*^%":
             tokens.append(char)
             printTokenConcluido(tokens)
+
+            # Operador consome 2 operandos e gera 1 resultado, então decrementa 1 do total de operandos
+            qnt_operandos -= 1
+
             return estadoInicial, ""
 
     raise ValueError(f"{erroText} erro lendo '{char}' ou lista '{lista}' indexado em {index}{RESET}")
 
 def estadoParenteses(char, tokens):
+    if len(tokens) == 0 and char == ')':
+        raise ValueError(f"{erroText} parêntese de fechamento ')' no inicio da linha{RESET}")
+
     global balance
     if char == '(': balance += 1
     elif char == ')': balance -= 1
